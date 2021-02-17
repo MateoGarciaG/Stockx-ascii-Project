6aea4348d17181fe7f82a1928c879c9ba04fb74c
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-02-17 18:32:39 +0100

    test(SneakerTest): :white_check_mark: Se realizo la estructura Basica para los casos Test de SneakerTest, obviamente puede cambiar, y hay dos casos test, uno para e constructor y otro para el toString() de un objeto tipo Item

--- src/main/java/edu/pingpong/stockx/domain/item/Sneaker.java
+++ src/main/java/edu/pingpong/stockx/domain/item/Sneaker.java
@@ -2,4 +2,59 @@ package edu.pingpong.stockx.domain.item;
 
 public class Sneaker implements Item {
     
+    private String style = null;
+    private String name = null;
+    private int sale;
+    private int ask;
+    private int bid;
+
+
+    public Sneaker(String style, String name) {
+        this.style = style;
+        this.name = name;
+
+    }
+
+
+    public String getStyle() {
+        return style;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
+    public int getSale() {
+        return sale;
+    }
+
+    public void setSale(int sale) {
+        this.sale = sale;
+    }
+
+
+
+    public int getAsk() {
+        return ask;
+    }
+
+    public void setAsk(int ask) {
+        this.ask = ask;
+    }
+
+    public int getBid() {
+        return bid;
+    }
+
+    public void setBid(int bid) {
+        this.bid = bid;
+    }
+
+    @Override
+    public String toString() {
+        //return String.format("%s\n%s", this.getName(), this.getStyle());
+        return "";
+    }
+
 }
--- src/test/java/edu/pingpong/stockx/domain/item/SneakerTest.java
+++ src/test/java/edu/pingpong/stockx/domain/item/SneakerTest.java
@@ -5,5 +5,26 @@ import org.junit.*;
 
 public class SneakerTest {
     
-    
+
+    private Item sneaker = null;
+
+    @Before
+    public void setupSneaker() {
+
+        this.sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
+    }
+
+    @Test
+    public void constructorTest() {
+
+        assertNotNull(this.sneaker);
+    }
+
+    @Test
+    public void toStringTest() {
+
+        assertEquals("Jordan 1 Retro High Dark Mocha\n555088-105", this.sneaker.toString());
+    }
+
+
 }
