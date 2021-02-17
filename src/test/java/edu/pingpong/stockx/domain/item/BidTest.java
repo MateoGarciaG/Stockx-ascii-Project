d004ccd17ebf50d8b319d65978b0fe99fe88e67a
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-02-17 19:35:17 +0100

    test: :white_check_mark: Se ha agregado la estructura basica de Casos Test de la clase BidTest, con dos 4 casos test: 2 para chequear el método size() y otros 2 para chequear value()

--- src/test/java/edu/pingpong/stockx/domain/item/BidTest.java
+++ src/test/java/edu/pingpong/stockx/domain/item/BidTest.java
@@ -5,5 +5,36 @@ import org.junit.*;
 
 public class BidTest {
     
-    
+    Offer bid = null;
+
+    @Before
+    public void setupBid() {
+
+        String size = "13";
+        Integer ask = 550;
+
+        this.bid = new Bid(size, ask);
+    }
+
+    @Test
+    public void constructorBid() {
+
+        assertNotNull(this.bid);
+    }
+
+    @Test
+    public void sizeTest() {
+
+        assertEquals("13", this.bid.size());
+        assertEquals(550, this.bid.value());
+        
+        // Otro Objeto bid
+        String size = "6";
+        Integer ask = 200;
+
+        this.bid = new Bid(size, ask);
+
+        assertEquals("6", this.bid.size());
+        assertEquals(200, this.bid.value());
+    }
 }
